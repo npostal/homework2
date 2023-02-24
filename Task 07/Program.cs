f0cee0080e84de751bc63d4a957ee22b21d40845
Nikita <npost.al@yandex.ru>
2023-02-24 20:10:28 +0300

    T7

--- Task 07/Program.cs
+++ Task 07/Program.cs
@@ -5,3 +5,15 @@
 // 782 -> 2
 // 918 -> 8
 
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number > 99 && number < 1000)
+{
+    int result = number % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Нужно ввести трёхзначное число");
+}
