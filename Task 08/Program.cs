2f41f99ae94bb69fed9cde899fe00a54eb24a1db
Nikita <npost.al@yandex.ru>
2023-02-24 20:53:39 +0300

    Выполнено

--- Task 08/Program.cs
+++ Task 08/Program.cs
@@ -5,3 +5,21 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+
+Console.WriteLine("Введите целое число:");
+int i = 1;
+bool not = true;
+int number1 = Convert.ToInt32(Console.ReadLine());
+while (i <= number1)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел");
+}
