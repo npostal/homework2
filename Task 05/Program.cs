0ee805e6ecdfabefc51ee1c114388afaacd649c3
Nikita <npost.al@yandex.ru>
2023-02-24 20:17:44 +0300

    Выполнено

--- Task 05/Program.cs
+++ Task 05/Program.cs
@@ -4,6 +4,7 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> " -2, -1, 0, 1, 2"
 
+
 Console.WriteLine("Введите натуральное число:");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number > 0)
