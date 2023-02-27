6c06e6472363a4357429fdf8f9f6d612802db6ed
Nikita <npost.al@yandex.ru>
2023-02-27 22:50:35 +0300

    Выполнено

---
+++ Task 14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2 .проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool checkNumber = CheckNumber(number);
+Console.WriteLine(checkNumber ? "Да" : "Нет");
+
+bool CheckNumber(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
