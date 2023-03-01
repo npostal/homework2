f8eedf1e406342907075173c2d5a435589848e28
Nikita <npost.al@yandex.ru>
2023-03-01 22:00:14 +0300

    Выполнено

---
+++ Task 11/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая 
+//1. выводит случайное трёхзначное число и 
+//2. удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трёхзначное число -> {number}");
+int sumDigit = SumDigit(number);
+
+Console.WriteLine($"Число после удаления второй цифры случайного числа -> {sumDigit}");
+
+int SumDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int sumD = firstDigit * 10 + lastDigit;
+    return sumD;
+}
+
+
+
