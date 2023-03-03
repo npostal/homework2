5f44eb3c558314cd3814e5f4b0afcfb4912e1565
Nikita <npost.al@yandex.ru>
2023-03-03 23:55:35 +0300

    Выполнено

---
+++ Task 23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая 
+// принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    TableCube(number);
+}
+else
+{
+    Console.WriteLine("Введено некорректное число");
+}
+
+void TableCube(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+        Console.WriteLine($"{i,4} -> {i * i * i,1}");
+        i++;
+    }
+}
