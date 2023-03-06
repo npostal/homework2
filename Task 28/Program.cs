5ebc6efa35cbf8e33ac4f6a16749d9d3b3318b95
Nikita <npost.al@yandex.ru>
2023-03-06 21:42:37 +0300

    Выполнено

---
+++ Task 28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая
+// 1. принимает на вход число N и 
+// 2. выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int multiplication = Multiplication(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {multiplication}");
+
+int Multiplication(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            mult = mult * i;
+        }
+    }
+    return mult;
+}
