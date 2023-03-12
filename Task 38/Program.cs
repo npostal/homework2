acc34cd3e27e850f8ec867718957618ab557213f
Nikita <npost.al@yandex.ru>
2023-03-12 21:48:50 +0300

    Невыполнено

---
+++ Task 38/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+Console.WriteLine("Введите размер массива");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[arraySize];
+FillArray(numbers);
+int min = Convert.ToInt32;
+int max = Convert.ToInt32;
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000) / 100;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}; ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("] -> ");
+}
+
+PrintArray(numbers);
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+Console.WriteLine(max - min);
+
+
