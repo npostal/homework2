02430f71f3a3292ec145a55352b3be9c8481fbd4
Nikita <npost.al@yandex.ru>
2023-03-13 21:12:00 +0300

    Выполнено

---
+++ Task 42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(BinareCode(number));
+
+string BinareCode(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result;
+}
