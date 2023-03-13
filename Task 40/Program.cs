6a8dba3c05a47a36647bf1b59f5ddbae0886a60c
Nikita <npost.al@yandex.ru>
2023-03-13 21:20:14 +0300

    Выполнено

---
+++ Task 40/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Введите первую сторону ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую сторону ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью сторону ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CheckTriangle(a, b, c)? "Да" : "Нет");
+
+bool CheckTriangle(int a, int b, int c)
+{
+    if (a < b + c)
+    {
+        if (b < a + c)
+        {
+            if (c < a + b) return true;
+            else return false;
+        }
+        else return false;
+    }
+    else return false;
+}
