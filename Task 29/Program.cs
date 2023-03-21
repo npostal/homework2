2766d01300652d10e38d068c2148e9f946742a6d
Nikita <npost.al@yandex.ru>
2023-03-21 18:31:09 +0300

    Выполнено

---
+++ Task 29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая 
+// 1. задаёт массив из 8 элементов и 
+// 2. выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
