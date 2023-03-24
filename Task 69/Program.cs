9de66f9a279a5c4cc793153ad22ddcb0aa61a734
Nikita <npost.al@yandex.ru>
2023-03-24 09:04:32 +0300

    Выполнено

---
+++ Task 69/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int numberA = Input("Введите целое положительное число A: ");
+int numberB = Input("Введите целое положительное число B: ");
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int multi = MultiNumbers(numberA, numberB);
+Console.Write(multi);
+
+int MultiNumbers(int a, int b)
+ {
+     if (b == 0) return 1;
+     else return checked (a * MultiNumbers(a, b - 1));
+ }
